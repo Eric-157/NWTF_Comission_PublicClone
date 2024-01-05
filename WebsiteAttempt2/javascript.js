7beb86f088e05db291f6c51a1234662e6f798e86
Eric Edwards <59770361+Eric-157@users.noreply.github.com>
2024-01-04 21:13:38 -0500

    Cloned the website
    Removed any private or personal information that was in the privated repository

---
+++ WebsiteAttempt2/javascript.js
@@ -0,0 +1,14 @@
+//////////////////////////////////////////////////
+//  NWTF Website                                //
+//                                              //
+//  Javascript file for Menu                    //
+//                                              //
+//  Authors: Ryan Griffin and Eric Edwards      //
+//                                              //
+//  Date: 11/19/2021                            //
+//////////////////////////////////////////////////
+
+// Button for mobile navigation drop down menu//
+function button_press() {
+  document.querySelector("nav").classList.toggle("open");
+}
---
+++ WebsiteAttempt2/readme!.txt
@@ -0,0 +1,7 @@
+
+EMAIL PASSWORD: nwtfswya2017
+EMAIL ADDRESS: nwtfswampyangob@gmail.com
+
+New Email Address (Made by Eric)
+Email Address: TriCounty.NWTF.MA@gmail.com
+Email Password: Tr1-C0unty.2021-NWTF
